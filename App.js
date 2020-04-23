6da85cae96c20948a94c8c4ed18c16a64956b252
Jorge Eduardo <corpjorge@hotmail.com>
2020-04-22 22:14:30 -0500

    update app.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
-import { Slider, Block  } from 'galio-framework';
+import { Button, NavBar  } from 'galio-framework';
 
 
 
@@ -9,13 +9,8 @@ export default function App() {
 
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-          <Block flex>
-              <Slider
-                  maximumValue={30}
-                  value={10}
-                  onSlidingcomplete={() => this.registerTheValue()}
-              />
-          </Block>
+    
+             <Button shadowless />
 
       </View>
   );

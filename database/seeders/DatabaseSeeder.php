bd98d3cef8df36d1ed964650c7d5dfa4a1e5f1a6
Jorge Eduardo <corpjorge@hotmail.com>
2021-04-29 12:51:58 -0500

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            UserSeeder::class,
+            //UserSeeder::class,
             DepartamentoSeeder::class,
             MunicipioSeeder::class
         ]);
